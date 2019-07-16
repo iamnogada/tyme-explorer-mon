41e67f38f037b0b422a705cc87f61331947addc9
myguddy <seungil.han@gmail.com>
2019-07-16 15:37:05 +0900

    1

--- mongo.js
+++ mongo.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose')
-
+const fs = require('fs')
+const ca = [fs.readFileSync("./config/rds-combined-ca-bundle.pem")];
 const dbconfig = {
-	endpoint: "mongodb://tyme:q1w2e3r4@tyme-mainnet.cluster-cvxrevdoop1u.ap-northeast-2.docdb.amazonaws.com:27017/test?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred",
+    endpoint: "mongodb://tyme:q1w2e3r4@tyme-mainnet.cluster-cvxrevdoop1u.ap-northeast-2.docdb.amazonaws.com:27017/test?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred",
     name: "tyme-mainnet",
     options: {
-	sslValidate: true,
-        sslCA:ca,
-	useNewUrlParser: true
+        ssl: true,
+        sslValidate: true,
+        sslCA: ca,
+        useNewUrlParser: true
     }
 }
 let uri = "mongodb://tyme:q1w2e3r4@tyme-mainnet.cluster-cvxrevdoop1u.ap-northeast-2.docdb.amazonaws.com:27017/tyme-mainnet?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred"
-mongoose.connect(dbconfig,dbconfig.options)
-.then(()=>{
-    console.log('connected');
-    
-})
-.catch(e=>{
-    console.log('error:'+e.message);
-    
-})
+mongoose.connect(dbconfig, dbconfig.options)
+    .then(() => {
+        console.log('connected');
+
+    })
+    .catch(e => {
+        console.log('error:' + e.message);
+
+    })
