c60e4fc892912ce157b77a156cad97759e4467a7
myguddy <seungil.han@gmail.com>
2019-07-16 15:27:56 +0900

    test

--- mongo.js
+++ mongo.js
@@ -1,1 +1,21 @@
-//mogotest
+const mongoose = require('mongoose')
+
+const dbconfig = {
+	endpoint: "mongodb://tyme:q1w2e3r4@tyme-mainnet.cluster-cvxrevdoop1u.ap-northeast-2.docdb.amazonaws.com:27017/test?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred",
+    name: "tyme-mainnet",
+    options: {
+	sslValidate: true,
+        sslCA:ca,
+	useNewUrlParser: true
+    }
+}
+let uri = "mongodb://tyme:q1w2e3r4@tyme-mainnet.cluster-cvxrevdoop1u.ap-northeast-2.docdb.amazonaws.com:27017/tyme-mainnet?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred"
+mongoose.connect(dbconfig,dbconfig.options)
+.then(()=>{
+    console.log('connected');
+    
+})
+.catch(e=>{
+    console.log('error:'+e.message);
+    
+})
